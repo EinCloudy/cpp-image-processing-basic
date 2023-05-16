0a88d6b262941768480a42c4e92a3c0e9f301ed7
Joel Obi <2212833@stud.hs-mannheim.de>
2023-05-16 17:52:31 +0200

    made code simpler

--- ImageProcessing.cpp
+++ ImageProcessing.cpp
@@ -34,57 +34,56 @@ public:
     void edgeDetection() {
         printf("Applying edge detection\n");
 
+        // Get the image dimensions
+        int width = image->getWidth();
+        int height = image->getHeight();
+
         // Create a new array to store the filtered pixels
         Pixel *pixels = image->getPixels();
-        Pixel *newPixels = (Pixel*)malloc(sizeof(Pixel) * image->getWidth() * image->getHeight());
+        Pixel *newPixels = new Pixel[width * height];
 
         // Apply the filter to each pixel
-        for(int i = 0; i < image->getWidth() * image->getHeight(); i++) {
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                int sumR = 0;
+                int sumG = 0;
+                int sumB = 0;
 
-            // The x coordinate is the remainder of i divided by the width
-            int x = i % image->getWidth();
+                // Apply the filter to the pixels around the current pixel
+                for(int j = -1; j <= 1; j++) {
+                    for(int i = -1; i <= 1; i++) {
+                        // The value of the filter at the current position
+                        int filterValue = doubleFilter[j + 1][i + 1];
 
-            // The y coordinate is the integer division of i by the width
-            int y = i / image->getWidth();
+                        // The x and y coordinates of the pixel to apply the filter to
+                        int x2 = x + i;
+                        int y2 = y + j;
 
-            int sumR = 0;
-            int sumG = 0;
-            int sumB = 0;
-
-            // Apply the filter to the pixels around the current pixel
-            for(int j = -1; j <= 1; j++) {
-                for(int k = -1; k <= 1; k++) {
-                    // The value of the filter at the current position
-                    int filterValue = doubleFilter[k + 1][j + 1];
-
-                    // The x and y coordinates of the pixel to apply the filter to
-                    int x2 = x + j;
-                    int y2 = y + k;
-
-                    // Only apply the filter to pixels within the image
-                    if(x2 >= 0 && x2 < image->getWidth() && y2 >= 0 && y2 < image->getHeight()) {
-                        sumR += pixels[y2 * image->getWidth() + x2].r * filterValue;
-                        sumG += pixels[y2 * image->getWidth() + x2].g * filterValue;
-                        sumB += pixels[y2 * image->getWidth() + x2].b * filterValue;
+                        // Only apply the filter to pixels within the image
+                        if(x2 >= 0 && x2 < width && y2 >= 0 && y2 < height) {
+                            Pixel pixel = pixels[y2 * width + x2];
+                            sumR += pixel.r * filterValue;
+                            sumG += pixel.g * filterValue;
+                            sumB += pixel.b * filterValue;
+                        }
                     }
                 }
-            }
 
-            // Clamp the values to the range [0, 255]
-            newPixels[i].r = max(0, min(255, sumR));
-            newPixels[i].g = max(0, min(255, sumG));
-            newPixels[i].b = max(0, min(255, sumB));
+                // Clamp the values to the range [0, 255]
+                newPixels[y * width + x].r = max(0, min(255, sumR));
+                newPixels[y * width + x].g = max(0, min(255, sumG));
+                newPixels[y * width + x].b = max(0, min(255, sumB));
+            }
         }
 
         // Update the original image with the filtered pixels
-        for(int i = 0; i < image->getWidth() * image->getHeight(); i++) {
-            pixels[i].r = newPixels[i].r;
-            pixels[i].g = newPixels[i].g;
-            pixels[i].b = newPixels[i].b;
+        for(int i = 0; i < width * height; i++) {
+            pixels[i] = newPixels[i];
         }
 
         // Free the memory allocated for the new pixels
-        free(newPixels);
+        delete[] newPixels;
+
         printf("Applied edge detection\n");
     }
 };
